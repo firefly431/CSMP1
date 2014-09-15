af47aa214f41ef1c5b656428765974b135ff6a89
thiensonho <thiensonho@gmail.com>
2014-09-15 13:33:40 -0500

    created gameInterface class
    added paint method

---
+++ cs/tetris/gameInterface.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cs.tetris;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author s544545
+ */
+public class gameInterface extends JFrame{
+
+}
