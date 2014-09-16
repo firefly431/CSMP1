e04eb6e6de854e0da6d97317b61400aeaf13f766
firefly431 <firefly431.awesome@gmail.com>
2014-09-16 13:34:19 -0500

    begin working on interface

--- cs/tetris/Board.java
+++ cs/tetris/Board.java
@@ -15,17 +15,24 @@ public class Board {
     public static int BOARD_HEIGHT = 20;
     public static int PIECE_SIZE = 10; // 10 px
 
-   int[][] grid = new int[BOARD_HEIGHT][BOARD_WIDTH];
+    int[][] grid = new int[BOARD_HEIGHT][BOARD_WIDTH];
 
-   public Board() {
+    public Board() {
+        fill();
+    }
 
-   }
+    /**
+     * Fill with -1s
+     */
+    public void fill() {
+        for(int y = 0; y < BOARD_HEIGHT; y ++) {
+            for(int x = 0; x < BOARD_WIDTH; x ++) {
+                grid[y][x] = -1;
+            }
+        }
+    }
 
-   public void fill() {
-       for(int y = 0; y < BOARD_HEIGHT; y ++) {
-           for(int x = 0; x < BOARD_WIDTH; x ++) {
-               grid[y][x] = -1;
-           }
-       }
-   }
+    public int get(int x, int y) {
+        return grid[y][x];
+    }
 }
--- cs/tetris/GamePanel.java
+++ cs/tetris/GamePanel.java
@@ -14,6 +14,17 @@ import java.awt.event.*;
  * @author s506571
  */
 public class GamePanel extends JPanel implements KeyListener {
+    public static int BOARD_PIXEL_WIDTH = Board.BOARD_WIDTH * Board.PIECE_SIZE;
+    public static int BOARD_PIXEL_HEIGHT = Board.BOARD_HEIGHT * Board.PIECE_SIZE;
+    public static int BOARD_X = (GameFrame.WINDOW_WIDTH - BOARD_PIXEL_WIDTH) / 2;
+    public static int BOARD_Y = (GameFrame.WINDOW_HEIGHT - BOARD_PIXEL_HEIGHT) / 2;
+
+    public void paint(Graphics g) {
+        for (int y = 0; y < Board.BOARD_HEIGHT; y++) {
+            //
+        }
+    }
+
     public void keyTyped(KeyEvent e) {
     }
 
