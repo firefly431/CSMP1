c5712d7ec1117c3fefde9c6af4059cdec631af41
thiensonho <thiensonho@gmail.com>
2014-09-18 13:32:33 -0500

    addded stuff
    GameOver screen
    lose paint
    deleted test colors

---
+++ cs/tetris/GameOver.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cs.tetris;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+/**
+ *
+ * @author s544545
+ */
+public class GameOver extends StatePanel {
+
+    protected static BufferedImage loseImg = null;
+
+    public GameOver() {
+        //
+    }
+
+    public static void init() {
+        try {
+            loseImg = ImageIO.read(new File("lose.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Could not load menu image");
+        }
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        if (loseImg != null)
+            g.drawImage(loseImg, 0, 0, this);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            GameFrame.get().transition(new MainMenu());
+        }
+    }
+}
