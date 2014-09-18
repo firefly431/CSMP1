7cf9f7ba8bfb41f899ee07657adab9d8d6f0205e
firefly431 <firefly431.awesome@gmail.com>
2014-09-18 13:16:49 -0500

    Remove debug code

--- cs/tetris/GamePanel.java
+++ cs/tetris/GamePanel.java
@@ -134,7 +134,6 @@ public class GamePanel extends StatePanel implements ActionListener {
             for (Point x : piece.coords) {
                 int tx = x.x + piece.position.x;
                 int ty = x.y + piece.position.y;
-                System.out.printf("Setting (%d, %d) to %d", tx, ty, piece.index);
                 board.set(tx, ty, piece.index);
             }
             // replace piece
